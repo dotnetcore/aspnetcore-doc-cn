271385d9fad7cb3c9286b712bb4fefe795f703ac
seay <seayxu@163.com>
2016-06-13 13:42:09 +0800

    updated at:2016-06-13 13:42:09

--- sync.sh
+++ sync.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+echo "change dir..."
 cd ../src
+echo "dir:`pwd`"
+
+echo -e '\n'
+
+echo "git pull repo from parent..."
 git pull parent dev
+echo "git pull repo from parent complated!"
+
+echo -e '\n'
+
+echo "git commit repo into local..."
 git add -A
 git commit -m "updated at:$(date '+%Y-%m-%d %H:%M:%S')"
+echo "git commit repo into local complated!"
+
+echo -e '\n'
+
+echo "git push repo to origin...!"
 git push origin dev
+echo "git push repo to origin complated!"
